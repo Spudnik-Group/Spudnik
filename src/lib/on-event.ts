aa7081a085abed114f09cb056a320c33f6a479ef
Matt Carroll <admin@develogix.com>
2018-01-10 21:05:22 -0500

    Fixed promises... typescript is odd, m'kay.

--- src/lib/on-event.ts
+++ src/lib/on-event.ts
@@ -2,10 +2,10 @@ import { Spudnik } from "../spudnik";
 
 module.exports = (Spudnik: Spudnik) => {
 	require('./on-event/ready')(Spudnik);
-	// require('./on-event/message')(Spudnik);
-	// require('./on-event/guild-create')(Spudnik);
-	// require('./on-event/guild-delete')(Spudnik);
-	// require('./on-event/guild-member-add')(Spudnik);
-	// require('./on-event/guild-member-leave')(Spudnik);
-	// require('./on-event/disconnected')(Spudnik);
+	require('./on-event/message')(Spudnik);
+	require('./on-event/guild-create')(Spudnik);
+	require('./on-event/guild-delete')(Spudnik);
+	require('./on-event/guild-member-add')(Spudnik);
+	require('./on-event/guild-member-leave')(Spudnik);
+	require('./on-event/disconnected')(Spudnik);
 }
