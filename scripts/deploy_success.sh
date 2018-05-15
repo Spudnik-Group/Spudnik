91a0f195dcdc5c0653f99984dc5720b994ce5f11
Nate Richardson <hello@naterichardson.com>
2018-05-15 15:24:28 -0400

    added honeybadger deployment notifications

---
+++ scripts/deploy_success.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo 'Sending Honeybadger Webhook';
+curl -g "https://api.honeybadger.io/v1/deploys?deploy[environment]=production&deploy[local_username]=Spudnik-Group&deploy[revision]=${TRAVIS_COMMIT:0:7}&deploy[repository]=https://github.com/Spudnik-Group/Spudnik&api_key=$HONEYBADGER_API_KEY"
