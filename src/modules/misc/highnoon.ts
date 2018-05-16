984af56aa570cf3782bed391ca6a293e8a182039
Nate Richardson <hello@naterichardson.com>
2018-05-15 22:48:58 -0400

    Revert "fixed highnoon module to have standardized request/error handling"
    This reverts commit fa14691c1f4cb38821abee31d5c19d32cea0c78f.

--- src/modules/misc/highnoon.ts
+++ src/modules/misc/highnoon.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
-import * as rp from 'request-promise';
+import * as request from 'request';
 import { RequestResponse } from 'request';
 import { sendSimpleEmbeddedError, sendSimpleEmbeddedImage, sendSimpleEmbeddedMessage } from '../../lib/helpers';
 
@@ -40,30 +40,16 @@ export default class HighNoonCommand extends Command {
 	 * @memberof HighNoonCommand
 	 */
 	public async run(msg: CommandMessage): Promise<Message | Message[]> {
-		const response = await sendSimpleEmbeddedMessage(msg, 'Loading...');
-		const options = {
-			method: 'GET',
+		request({
 			uri: 'http://imgs.xkcd.com/comics/now.png',
 			followAllRedirects: true,
-			json: true,
-		};
-
-		rp(options)
-			.then((resp) => {
-				if (resp.uri.href) {
-					msg.delete();
-					return sendSimpleEmbeddedImage(msg, resp.uri.href.toString());
-				} else {
-					msg.delete();
-					msg.client.emit('warn', `Unsuccessful request.\nCommand: 'highnoon'\nContext: ${resp}`);
-					return sendSimpleEmbeddedError(msg, 'There was an error with the request. Try again?', 300);
-				}
-			})
-			.catch((err) => {
-				msg.delete();
-				msg.client.emit('error', `Error with command 'highnoon'\nError: ${err}`);
-				return sendSimpleEmbeddedError(msg, 'There was an error with the request. Try again?', 300);
-			});
-		return response;
+		}, (err: Error, resp: RequestResponse) => {
+			if (resp.request.uri.href) {
+				return sendSimpleEmbeddedImage(msg, resp.request.uri.href.toString());
+			} else {
+				return sendSimpleEmbeddedError(msg, 'There was an error with the request. Try again?');
+			}
+		});
+		return sendSimpleEmbeddedMessage(msg, 'Loading...');
 	}
 }
