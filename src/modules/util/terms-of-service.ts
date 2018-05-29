f2f4ccc7e48c035c8cd050ceb28f7bb6dfd3b9fc
Matt Carroll <admin@develogix.com>
2018-05-29 12:50:40 -0400

    add oneLine function to the details for TOS module

--- src/modules/util/terms-of-service.ts
+++ src/modules/util/terms-of-service.ts
@@ -1,3 +1,4 @@
+import { oneLine } from 'common-tags';
 import { Channel, Message, MessageEmbed } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 import { getEmbedColor } from '../../lib/custom-helpers';
@@ -40,8 +41,21 @@ export default class TermsOfServiceCommand extends Command {
 				}
 			],
 			description: 'Used to configure the Terms of Service for a guild.',
-			details: 'syntax: `!tos (channel|title|body|list) [channel mention | message number] [text]\n`channel mention` - Sets the channel to display the terms of service in.\n`(title) (info block number) (text)` - Edit the title of a terms of service info block.\n`(body) (info block number) (text)` - Edit the body of a terms of service info block.\n`list` - returns all the terms of service info blocks.\n\nManage Guild permission required.',
-			examples: ['!tos channel #channelMention', '!tos title 1 Interesting title', '!tos body 1 Interesting body text', '!tos list'],
+			details: oneLine`
+				syntax: \`!tos(channel|title|body|list) [channel mention | message number][text]\n
+				\n
+				\`channel mention\` - Sets the channel to display the terms of service in.\n
+				\`title (info block number) (text)\` - Edit the title of a terms of service info block.\n
+				\`body (info block number) (text)\` - Edit the body of a terms of service info block.\n
+				\`list\` - returns all the terms of service info blocks.\n
+				\n
+				Manage Guild permission required.')`,
+			examples: [
+				'!tos channel #channelMention',
+				'!tos title 1 Interesting title',
+				'!tos body 1 Interesting body text',
+				'!tos list'
+			],
 			group: 'util',
 			guildOnly: true,
 			memberName: 'tos',
