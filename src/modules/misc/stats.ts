8558d8a1520dc2195735a9621853f5e436c4eae7
Nate Richardson <hello@naterichardson.com>
2018-09-24 11:17:19 -0400

    updated stats command. resolves #167

--- src/modules/misc/stats.ts
+++ src/modules/misc/stats.ts
@@ -1,5 +1,5 @@
 import { stripIndents } from 'common-tags';
-import { Guild, Message } from 'discord.js';
+import { Guild, Message, MessageEmbed } from 'discord.js';
 import { Command, CommandMessage, CommandoClient } from 'discord.js-commando';
 import * as moment from 'moment';
 import 'moment-duration-format';
@@ -17,7 +17,7 @@ interface IDurationSettings {
 }
 
 // tslint:disable-next-line:no-var-requires
-const { version }: { version: string } = require('../../../package');
+const { version, dependencies }: { version: string, dependencies: any } = require('../../../package');
 
 /**
  * Post statistics about the bot.
@@ -60,36 +60,34 @@ export default class StatsCommand extends Command {
 	 */
 	public async run(msg: CommandMessage): Promise<Message | Message[]> {
 		const duration: IDuration = moment.duration(this.client.uptime) as IDuration;
-		return msg.embed({
-			color: getEmbedColor(msg),
-			description: '**Spudnik Statistics**',
-			fields: [
-				{
-					inline: true,
-					name: '❯ Uptime',
-					value: duration.format('d[ d] h[ h] m[ m] s[ s]')
-				},
-				{
-					inline: true,
-					name: '❯ Memory usage',
-					value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`
-				},
-				{
-					inline: true,
-					name: '❯ General Stats',
-					value: stripIndents`
+		let statsEmbed: MessageEmbed = new MessageEmbed()
+			.setColor(getEmbedColor(msg))
+			.setDescription('**Spudnik Statistics**')
+			.addField('❯ Uptime', duration.format('d[ d] h[ h] m[ m] s[ s]'), true)
+			.addField('❯ Process Stats', stripIndents`
+						• Memory Usage: ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB
+						• Node Version: ${process.version}
+						• Version: v${version}`, true)
+			.addField('❯ General Stats', stripIndents`
 						• Guilds: ${this.client.guilds.size}
 						• Channels: ${this.client.channels.size}
 						• Users: ${this.client.guilds.map((guild: Guild) => guild.memberCount).reduce((a: number, b: number): number => a + b)}
-					`
-				},
-				{
-					inline: true,
-					name: '❯ Version',
-					value: `v${version}`
-				}
-			],
-			thumbnail: { url: this.client.user.avatarURL }
-		});
+						• Commands: ${this.client.registry.commands.size}`, true)
+			.addField('❯ Server Stats', '', true)
+			.addField('❯ Home Server', `https://spudnik.io/support`, true)
+			.addField('❯ Source Code', `https://github.com/Spudnik-Group/Spudnik`, true)
+			.addField('❯ Dependencies', this.parseDependencies())
+			.setThumbnail(`${this.client.user.avatarURL}`);
+		return msg.embed(statsEmbed);
+	}
+
+	private parseDependencies() {
+		return Object.entries(dependencies).map((dep: any) => {
+			if (dep[1].startsWith('github:')) {
+				const repo = dep[1].replace('github:', '').split('/');
+				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#.+/, '')})`;
+			}
+			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
+		}).join(', ');
 	}
 }
