9d82cf2f770926fc3490e8271fb1f10aa238bfb0
Nate Richardson <hello@naterichardson.com>
2018-11-08 13:05:21 -0500

    follow up change for #186

--- src/lib/custom-helpers.ts
+++ src/lib/custom-helpers.ts
@@ -8,7 +8,11 @@ import { CommandMessage } from 'discord.js-commando';
  * @returns {number}
  */
 export function getEmbedColor(msg: CommandMessage): number {
-	let embedColor: number = parseInt(msg.client.provider.get(msg.guild.id, 'embedColor', '555555'), 16);
+	let embedColor: number = 555555;
+	if (msg.guild) {
+		embedColor = parseInt(msg.client.provider.get(msg.guild.id, 'embedColor', '555555'), 16);
+	} else {
+	}
 
 	// This shouldn't happen, but if it does, return the default embed color
 	if (embedColor > parseInt('FFFFFF', 16)) {
