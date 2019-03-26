f6ef002c56f239634ef45f46d848bc6c00017722
Nate Richardson <hello@naterichardson.com>
2019-03-26 07:53:01 -0400

    initial commit of bot list update calls

--- src/lib/handlers/handle-ready.ts
+++ src/lib/handlers/handle-ready.ts
@@ -2,14 +2,13 @@ import chalk from 'chalk';
 import { Guild, PresenceData } from 'discord.js';
 import { CommandoClient } from 'discord.js-commando';
 import { Configuration } from 'src/lib/spudnik';
+import * as rp from 'request-promise';
 
 export async function handleReady(version: string, client: CommandoClient, config: Configuration) {
-	const users: number = client.guilds.map((guild: Guild) => guild.memberCount).reduce((a: number, b: number): number => a + b);
-	const guilds: number = client.guilds.array().length;
 	const statuses: PresenceData[] = [
 		{
 			activity: {
-				name: `${client.commandPrefix}help | ${guilds} Servers`,
+				name: `${client.commandPrefix}help | ${client.guilds.array().length} Servers`,
 				type: 'PLAYING'
 			}
 		},
@@ -45,13 +44,13 @@ export async function handleReady(version: string, client: CommandoClient, confi
 		},
 		{
 			activity: {
-				name: `and Assisting ${users} users.`,
+				name: `and Assisting ${client.guilds.map((guild: Guild) => guild.memberCount).reduce((a: number, b: number): number => a + b)} users.`,
 				type: 'WATCHING'
 			}
 		},
 		{
 			activity: {
-				name: `and Assisting ${guilds} servers.`,
+				name: `and Assisting ${client.guilds.array().length} servers.`,
 				type: 'WATCHING'
 			}
 		},
@@ -63,47 +62,14 @@ export async function handleReady(version: string, client: CommandoClient, confi
 		}
 	];
 
-	console.log(chalk.magenta(`Logged into Discord! Serving in ${guilds} Discord servers`));
+	console.log(chalk.magenta(`Logged into Discord! Serving in ${client.guilds.array().length} Discord servers`));
 	console.log(chalk.blue('---Spudnik Launch Success---'));
 
 	// Update bot status, using array of possible statuses
 	let statusIndex: number = -1;
 	statusIndex = updateStatus(client, statuses, statusIndex);
 	setInterval(() => { statusIndex = updateStatus(client, statuses, statusIndex) }, config.statusUpdateInterval, true);
-	setInterval(() => updateStatusStats(config, client, statuses), config.botListUpdateInterval, true);
-}
-
-/**
- * Updates discord bot list stats and status messages on interval
- */
-const updateStatusStats = (config: Configuration, client: CommandoClient, statuses: PresenceData[]): PresenceData[] => {
-	const users: number = client.guilds.map((guild: Guild) => guild.memberCount).reduce((a: number, b: number): number => a + b);
-	const guilds: number = client.guilds.array().length;
-
-	// Update Statuses
-	statuses = statuses.filter((item: PresenceData) => {
-		if (item.activity && item.activity.type !== 'WATCHING') {
-			return true;
-		}
-		return false;
-	});
-
-	statuses.push(
-		{
-			activity: {
-				name: `and Assisting ${guilds} servers.`,
-				type: 'WATCHING'
-			}
-		},
-		{
-			activity: {
-				name: `and Assisting ${users} users.`,
-				type: 'WATCHING'
-			}
-		}
-	);
-
-	return statuses;
+	setInterval(() => updateBotListStats(config, client), config.botListUpdateInterval, true);
 }
 
 /**
@@ -119,4 +85,48 @@ const updateStatus = (client: CommandoClient, statuses: PresenceData[], statusIn
 	client.user.setPresence(statuses[statusIndex]);
 
 	return statusIndex;
+}
+
+/**
+ * Update bot list stats on interval
+ */
+const updateBotListStats = (config: Configuration, client: CommandoClient): void => {
+	// discord.bots.gg
+
+	// bots.ondiscord.xyz
+
+	// discordbots.org
+	if (process.env.DB_TOKEN) {
+		rp({
+			uri: `https://discordbots.org/api/bots/${client.user.id}/stats`,
+			method: 'POST',
+			headers: { Authorization: process.env.DB_TOKEN },
+			body: { server_count: client.guilds.size }
+		})
+		.then(() => client.log('- Posted statistics successfully', 'discordbots.org'))
+		.catch(() => client.log('Failed to post statistics', 'discordbots.org'))
+	}
+
+	// botsfordiscord.com
+	if (process.env.BOTSFORDISCORD_TOKEN) {
+		rp({
+			uri: `https://botsfordiscord.com/api/bots/${client.user.id}/stats`,
+			method: 'POST',
+			headers: { Authorization: process.env.BOTSFORDISCORD_TOKEN },
+			body: { server_count: client.guilds.size }
+		})
+		.then(() => client.log('- Posted statistics successfully', 'botsfordiscord.com'))
+		.catch(() => client.log('Failed to post statistics', 'botsfordiscord.com'))
+	}
+
+	if (process.env.DBL_TOKEN) {
+		rp({
+			uri: `https://discordbotlist.com/api/bots/${client.user.id}/stats`,
+			method: 'POST',
+			headers: { Authorization: `Bot ${process.env.DBL_TOKEN}` },
+			body: { guilds: client.guilds.size, users: client.users.size }
+		})
+		.then(() => client.log('- Posted statistics successfully', 'discordbotlist.com'))
+		.catch(() => client.log('Failed to post statistics', 'discordbotlist.com'))
+	}
 }
