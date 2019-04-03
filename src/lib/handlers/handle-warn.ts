2e42f99c8f7409fe8cc2f45e16f2ed12777fcb0d
Nate Richardson <hello@naterichardson.com>
2019-04-03 10:16:55 -0400

    debug fix

--- src/lib/handlers/handle-warn.ts
+++ src/lib/handlers/handle-warn.ts
@@ -4,12 +4,13 @@ import { stripIndents } from 'common-tags';
 import * as format from 'date-fns/format';
 
 export function handleWarn(err: Error, client: CommandoClient) {
-	const channel = client.channels.get(process.env.spud_issuelog) as TextChannel;
 	const message = stripIndents`
 	Caught **General Warning**!
 	**Time:** ${format(new Date(), 'MMMM Do YYYY [at] HH:mm:ss [UTC]Z')}
 	**Warning Message:** ${err}`;
-
-	channel.send(message);
+	if (!process.env.spud_debug) {
+		const channel = client.channels.get(process.env.spud_issuelog) as TextChannel;
+		channel.send(message);
+	}
 	console.warn(message);
 };
