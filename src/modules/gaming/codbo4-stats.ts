8923f1cb57437ed11f9dfd358a667c21bec3e678
Nate Richardson <hello@naterichardson.com>
2019-04-14 09:42:02 -0400

    all stat trackers

--- src/modules/gaming/codbo4-stats.ts
+++ src/modules/gaming/codbo4-stats.ts
@@ -9,36 +9,57 @@ const scoutID: string = process.env.spud_scoutid;
 const scoutSecret: string = process.env.spud_scoutsecret;
 
 /**
- * Returns Counter Strike: Global Offensive stats for a user on a specific platform.
+ * Returns Call of Duty: Black Ops 4 stats for a user on a specific platform.
  *
  * @export
- * @class CSGOStatsCommand
+ * @class CODBO4StatsCommand
  * @extends {Command}
  */
-export default class CSGOStatsCommand extends Command {
+export default class CODBO4StatsCommand extends Command {
 	/**
-	 * Creates an instance of CSGOStatsCommand.
+	 * Creates an instance of CODBO4StatsCommand.
 	 *
 	 * @param {CommandoClient} client
-	 * @memberof CSGOStatsCommand
+	 * @memberof CODBO4StatsCommand
 	 */
 	constructor(client: CommandoClient) {
 		super(client, {
-			aliases: ['csgo'],
+			aliases: ['codbo4', 'blops4'],
 			args: [
+				{
+					key: 'platform',
+					parse: (platform: string) => {
+						return platform.toLowerCase();
+					},
+					prompt: 'What platform should I look on?\nOptions are:\n* xbl\n* pc\n* psn',
+					type: 'string',
+					validate: (platform: string) => {
+						const allowedSubCommands = ['xbl', 'pc', 'psn'];
+						if (allowedSubCommands.indexOf(platform.toLowerCase()) !== -1) return true;
+
+						return 'You provided an invalid platform.';
+					}
+				},
 				{
 					key: 'username',
 					prompt: 'What is the profile name I\'m looking up?',
 					type: 'string'
 				}
 			],
-			description: 'Returns Counter Strike: Global Offensive stats for a user. Uses the TrackerNetwork API.',
-			details: 'syntax: \`!csgo-stats <username>\`',
-			examples: ['!csgo-stats phreakslayer'],
+			description: 'Returns Call of Duty: Black Ops 4 stats for a user on a specific platform. Uses the TrackerNetwork API.',
+			details: stripIndents`
+				syntax: \`!codbo4-stats <platform> <username>\`
+				
+				Platform must be one of: xbl, pc, psn
+			`,
+			examples: [
+				'!codbo4-stats xbl naterchrdsn',
+				'!codbo4-stats pc nebula-grey'
+			],
 			group: 'gaming',
 			guildOnly: true,
-			memberName: 'csgo-stats',
-			name: 'csgo-stats',
+			memberName: 'codbo4-stats',
+			name: 'codbo4-stats',
 			throttling: {
 				duration: 3,
 				usages: 2
@@ -47,18 +68,19 @@ export default class CSGOStatsCommand extends Command {
 	}
 
 	/**
-	 * Run the "csgo-stats" command.
+	 * Run the "codbo4-stats" command.
 	 *
 	 * @param {CommandoMessage} msg
-	 * @param {{ username: string }} args
+	 * @param {{ platform: string, username: string }} args
 	 * @returns {(Promise<Message | Message[]>)}
-	 * @memberof CSGOStatsCommand
+	 * @memberof CODBO4StatsCommand
 	 */
-	public async run(msg: CommandoMessage, args: { username: string }): Promise<Message | Message[]> {
-		const csgoEmbed: MessageEmbed = new MessageEmbed({
+	public async run(msg: CommandoMessage, args: { platform: string, username: string }): Promise<Message | Message[]> {
+		const platform = args.platform === 'pc' ? 'battlenet' : args.platform;
+		const codbo4Embed: MessageEmbed = new MessageEmbed({
 			author: {
-				icon_url: 'https://i.imgur.com/UBjioW8.png',
-				name: 'CS:GO Stats',
+				icon_url: 'https://i.imgur.com/90HVaib.png',
+				name: 'COD:BLOPS4 Stats',
 				url: 'https://scoutsdk.com/'
 			},
 			color: getEmbedColor(msg),
@@ -74,26 +96,26 @@ export default class CSGOStatsCommand extends Command {
 		});
 
 		startTyping(msg);
-		const search = await Scout.players.search(args.username, 'steam', null, games.csgo.id, true, true);
+		const search = await Scout.players.search(args.username, platform, null, games.codbo4.id, true, true);
 		if (search.results.length) {
 			const matches = search.results.filter((result: any) => result.player);
 			if (matches.length) {
 				// TODO: change this to allow selection of a result
 				const firstMatch = matches[0];
-				const playerStats = await Scout.players.get(games.csgo.id, firstMatch.player.playerId, '*');
-				csgoEmbed.setDescription(stripIndents`
+				const playerStats = await Scout.players.get(games.codbo4.id, firstMatch.player.playerId, '*');
+				codbo4Embed.setDescription(stripIndents`
 					**${firstMatch.persona.handle}**
 
 					${playerStats.metadata[1].name}: ${playerStats.metadata[1].displayValue}
 				`);
 				playerStats.stats.forEach((statObj: any) => {
 					if (!statObj.displayValue) return;
-					csgoEmbed.addField(statObj.metadata.name, statObj.displayValue, true);
+					codbo4Embed.addField(statObj.metadata.name, statObj.displayValue, true);
 				});
 				deleteCommandMessages(msg, this.client);
 				stopTyping(msg);
 
-				return msg.embed(csgoEmbed);
+				return msg.embed(codbo4Embed);
 			} else {
 				stopTyping(msg);
 	
--- src/modules/gaming/codwwii-stats.ts
+++ src/modules/gaming/codwwii-stats.ts
@@ -9,36 +9,57 @@ const scoutID: string = process.env.spud_scoutid;
 const scoutSecret: string = process.env.spud_scoutsecret;
 
 /**
- * Returns Counter Strike: Global Offensive stats for a user on a specific platform.
+ * Returns Call of Duty: WWII stats for a user on a specific platform.
  *
  * @export
- * @class CSGOStatsCommand
+ * @class CODWWIIStatsCommand
  * @extends {Command}
  */
-export default class CSGOStatsCommand extends Command {
+export default class CODWWIIStatsCommand extends Command {
 	/**
-	 * Creates an instance of CSGOStatsCommand.
+	 * Creates an instance of CODWWIIStatsCommand.
 	 *
 	 * @param {CommandoClient} client
-	 * @memberof CSGOStatsCommand
+	 * @memberof CODWWIIStatsCommand
 	 */
 	constructor(client: CommandoClient) {
 		super(client, {
-			aliases: ['csgo'],
+			aliases: ['codwwii'],
 			args: [
+				{
+					key: 'platform',
+					parse: (platform: string) => {
+						return platform.toLowerCase();
+					},
+					prompt: 'What platform should I look on?\nOptions are:\n* xbl\n* pc\n* psn',
+					type: 'string',
+					validate: (platform: string) => {
+						const allowedSubCommands = ['xbl', 'pc', 'psn'];
+						if (allowedSubCommands.indexOf(platform.toLowerCase()) !== -1) return true;
+
+						return 'You provided an invalid platform.';
+					}
+				},
 				{
 					key: 'username',
 					prompt: 'What is the profile name I\'m looking up?',
 					type: 'string'
 				}
 			],
-			description: 'Returns Counter Strike: Global Offensive stats for a user. Uses the TrackerNetwork API.',
-			details: 'syntax: \`!csgo-stats <username>\`',
-			examples: ['!csgo-stats phreakslayer'],
+			description: 'Returns Call of Duty: WWII stats for a user on a specific platform. Uses the TrackerNetwork API.',
+			details: stripIndents`
+				syntax: \`!codwwii-stats <platform> <username>\`
+				
+				Platform must be one of: xbl, pc, psn
+			`,
+			examples: [
+				'!codwwii-stats xbl naterchrdsn',
+				'!codwwii-stats pc nebula-grey'
+			],
 			group: 'gaming',
 			guildOnly: true,
-			memberName: 'csgo-stats',
-			name: 'csgo-stats',
+			memberName: 'codwwii-stats',
+			name: 'codwwii-stats',
 			throttling: {
 				duration: 3,
 				usages: 2
@@ -47,18 +68,19 @@ export default class CSGOStatsCommand extends Command {
 	}
 
 	/**
-	 * Run the "csgo-stats" command.
+	 * Run the "codwwii-stats" command.
 	 *
 	 * @param {CommandoMessage} msg
-	 * @param {{ username: string }} args
+	 * @param {{ platform: string, username: string }} args
 	 * @returns {(Promise<Message | Message[]>)}
-	 * @memberof CSGOStatsCommand
+	 * @memberof CODWWIIStatsCommand
 	 */
-	public async run(msg: CommandoMessage, args: { username: string }): Promise<Message | Message[]> {
-		const csgoEmbed: MessageEmbed = new MessageEmbed({
+	public async run(msg: CommandoMessage, args: { platform: string, username: string }): Promise<Message | Message[]> {
+		const platform = args.platform === 'pc' ? 'uplay' : args.platform;
+		const codwwiiEmbed: MessageEmbed = new MessageEmbed({
 			author: {
-				icon_url: 'https://i.imgur.com/UBjioW8.png',
-				name: 'CS:GO Stats',
+				icon_url: 'https://i.imgur.com/H7AGNoX.png',
+				name: 'codwwii Stats',
 				url: 'https://scoutsdk.com/'
 			},
 			color: getEmbedColor(msg),
@@ -74,26 +96,26 @@ export default class CSGOStatsCommand extends Command {
 		});
 
 		startTyping(msg);
-		const search = await Scout.players.search(args.username, 'steam', null, games.csgo.id, true, true);
+		const search = await Scout.players.search(args.username, platform, null, games.codwwii.id, true, true);
 		if (search.results.length) {
 			const matches = search.results.filter((result: any) => result.player);
 			if (matches.length) {
 				// TODO: change this to allow selection of a result
 				const firstMatch = matches[0];
-				const playerStats = await Scout.players.get(games.csgo.id, firstMatch.player.playerId, '*');
-				csgoEmbed.setDescription(stripIndents`
+				const playerStats = await Scout.players.get(games.codwwii.id, firstMatch.player.playerId, '*');
+				codwwiiEmbed.setDescription(stripIndents`
 					**${firstMatch.persona.handle}**
 
 					${playerStats.metadata[1].name}: ${playerStats.metadata[1].displayValue}
 				`);
 				playerStats.stats.forEach((statObj: any) => {
 					if (!statObj.displayValue) return;
-					csgoEmbed.addField(statObj.metadata.name, statObj.displayValue, true);
+					codwwiiEmbed.addField(statObj.metadata.name, statObj.displayValue, true);
 				});
 				deleteCommandMessages(msg, this.client);
 				stopTyping(msg);
 
-				return msg.embed(csgoEmbed);
+				return msg.embed(codwwiiEmbed);
 			} else {
 				stopTyping(msg);
 	
