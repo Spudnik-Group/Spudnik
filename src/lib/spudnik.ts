7593783dca68a3b209814b701ab8867a08424678
Joseph Moran III <joseph.moraniii@gmail.com>
2019-04-15 22:57:23 -0400

    Add overwatch stats

--- src/lib/spudnik.ts
+++ src/lib/spudnik.ts
@@ -96,6 +96,7 @@ export class Spudnik {
 				['random', 'Random'],
 				['ref', 'Reference'],
 				['roles', 'Role'],
+				['stats', 'Stats'],
 				['translate', 'Translate'],
 				['util', 'Utility'],
 				['util-required', 'Required Utility']
