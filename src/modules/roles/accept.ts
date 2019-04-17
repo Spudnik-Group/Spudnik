de21d92a58002d3657a528c34d3ca92a25ce5156
Matt Carroll <admin@develogix.com>
2019-04-17 11:35:47 -0400

    Move commands/docs to proper groups

--- src/modules/roles/accept.ts
+++ src/modules/roles/accept.ts
@@ -42,7 +42,7 @@ export default class AcceptCommand extends Command {
 				'!accept',
 				'!accept #channelMention'
 			],
-			group: 'feature',
+			group: 'roles',
 			guildOnly: true,
 			memberName: 'accept',
 			name: 'accept',
