bb3d4384df55db6633d835edbc7f1b97a290c953
Nate Richardson <hello@naterichardson.com>
2019-08-28 10:02:41 -0400

    defined default guild settings schema

--- src/lib/spudnik.ts
+++ src/lib/spudnik.ts
@@ -6,6 +6,57 @@ import * as http from 'http';
 const PORT = process.env.PORT || 1337;
 
 /**
+ * Define Default Schemas
+ * 
+ */
+Client.defaultPermissionLevels
+	.add(3, ({ guild, member }) => guild && member.permissions.has('KICK_MEMBERS'), { fetch: true })
+	.add(4, ({ guild, member }) => guild && member.permissions.has('BAN_MEMBERS'), { fetch: true })
+	// TODO: add staff feature
+	// .add(5, ({ guild, author }) => guild.settings['staff'].includes(author.id));
+Client.defaultGuildSchema
+	.add('channels', folder => folder
+		.add('botAnnounce', 'TextChannel')
+	)
+	.add('roles', folder => folder
+		// TODO: add botAdmin feature
+		// .add('botAdmin', 'Role')
+		// TODO: add staff feature
+		// .add('staff', 'Role')
+		.add('defaultRole', 'Role')
+	)
+	.add('features', folder => folder
+		.add('starboard', folder => folder
+			.add('starboardEnabled', 'boolean', { default: false })
+			.add('starboardChannel', 'TextChannel', { default: null })
+			.add('starboardTrigger', 'string', { default: '⭐' })
+		)
+		.add('welcome', folder => folder
+			.add('welcomeEnabled', 'boolean', { default: false })
+			.add('welcomeChannel', 'TextChannel', { default: null })
+			.add('welcomeMessage', 'string', { default: `` })
+		)
+		.add('goodbye', folder => folder
+			.add('goodbyeEnabled', 'boolean', { default: false })
+			.add('goodbyeChannel', 'TextChannel', { default: null })
+			.add('goodbyeMessage', 'string', { default: `` })
+		)
+		.add('modlog', folder => folder
+			.add('modlogChannel', 'TextChannel', { default: null })
+			.add('modlogEnabled', 'boolean', { default: false })
+		)
+		.add('tos', folder => folder
+			.add('tosChannel', 'TextChannel', { default: null })
+			.add('tosMessages', 'any', { array: true })
+		)
+		.add('embedColor', 'string', { default: '555555' })
+		.add('adblockEnabled', 'boolean', { default: false })
+		.add('deleteCommandMessages', 'boolean', { default: false })
+		.add('hasSendModLogMessage', 'boolean', { default: false })
+		.add('selfAssignableRoles', 'any', { array: true })
+	)
+
+/**
  * The Spudnik Discord Bot.
  *
  * @export
