9c61ea8c5a53d7baa38f4b10e9980af651b14c8e
Nate Richardson <hello@naterichardson.com>
2020-05-29 10:50:52 -0400

    alphabetized bot listings

--- src/commands/bot-info/upvote.ts
+++ src/commands/bot-info/upvote.ts
@@ -33,17 +33,20 @@ export default class UpvoteCommand extends Command {
 	public async run(msg: KlasaMessage): Promise<KlasaMessage | KlasaMessage[]> {
 		return msg.sendSimpleEmbed(stripIndents`
 			Upvote Spudnik for the Motherland!
-			* [discord.boats](https://discord.boats/bot/398591330806398989)
-			* [top.gg](https://top.gg/bot/398591330806398989)
+			* [arcane-center.xyz](https://arcane-center.xyz/bot/398591330806398989)
+			* [botlist.space](https://botlist.space/bot/398591330806398989)
 			* [botsfordiscord.com](https://botsfordiscord.com/bot/398591330806398989)
+			* [cloudlist.xyz](https://www.cloudlist.xyz/bots/398591330806398989)
 			* [discordbotlist.com](https://discordbotlist.com/bots/398591330806398989)
-			* [lbots.org](https://lbots.org/bots/398591330806398989)
-			* [listmybots.com](https://listmybots.com/bot/398591330806398989)
 			* [discordextremelist.xyz](https://discordextremelist.xyz/bots/398591330806398989)
+			* [listmybots.com](https://listmybots.com/bots/398591330806398989)
+			* [mythicalbots.xyz](https://mythicalbots.xyz/bot/398591330806398989)
+			* [top.gg](https://top.gg/bot/398591330806398989)
 
-			Review Spudnik for the Motherland!
-			* [discordapps.dev](https://discordapps.dev/en-GB/bots/398591330806398989)
+			Rate/Review Spudnik for the Motherland!
 			* [bots.ondiscord.xyz](https://bots.ondiscord.xyz/bots/398591330806398989)
+			* [discord.boats](https://discord.boats/bot/398591330806398989)
+			* [discordapps.dev](https://discordapps.dev/en-GB/bots/398591330806398989)
 		`);
 	}
 
