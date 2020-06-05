143cbbd6cf1bb8a0fdba6bcb3d0c829de44443d8
Nate Richardson <hello@naterichardson.com>
2020-06-05 19:46:54 -0400

    help command adjustments

--- src/commands/help/help.ts
+++ src/commands/help/help.ts
@@ -38,14 +38,15 @@ export default class HelpCommand extends Command {
 	 */
 	public run(msg: KlasaMessage, [command]: [Command]): Promise<KlasaMessage | KlasaMessage[]> {
 		const helpEmbed: MessageEmbed = baseEmbed(msg);
+		const prefix = msg.guild.settings.get(GuildSettings.Prefix);
 
 		if (command) {
 			helpEmbed
 				.setTitle(`__Command: **${command.name}**__`)
 				.addField('❯ Description', typeof command.description === 'function' ? command.description(msg.language) : command.description)
-				.addField('❯ Usage', command.usage.fullUsage(msg))
-				.addField('❯ Details', command.extendedHelp ? (typeof command.extendedHelp === 'function' ? command.extendedHelp(msg.language) : command.extendedHelp) : 'No extended help details.')
-				.addField('❯ Aliases', command.aliases.length > 0 ? command.aliases.join(', ') : 'None', true)
+				.addField('❯ Usage', `${prefix}${command.name} ${command.usage.usageString}`)
+				.addField('❯ Details', typeof command.extendedHelp === 'function' ? command.extendedHelp(msg.language) : command.extendedHelp)
+				.addField('❯ Aliases', command.aliases.length > 0 ? command.aliases.join(', ') : 'None')
 				.addField('❯ Category', `${command.category}`, true)
 				.addField('❯ BOT Permissions', command.requiredPermissions.bitfield ? `${getPermissionsFromBitfield(command.requiredPermissions).join('\n')}` : 'No extra perms required', true)
 				.addField('❯ User Permission Level', command.permissionLevel ? `${command.permissionLevel}: ${getPermissionsFromLevel(command.permissionLevel)}` : 'No special user perms required', true)
@@ -60,13 +61,13 @@ export default class HelpCommand extends Command {
 			.setTitle('**Help**')
 			.setThumbnail(`${this.client.user.avatarURL()}`)
 			.setDescription(stripIndents`
-					To get the list of command categories, type \`${msg.guild.settings.get(GuildSettings.Prefix)}commands\`.
-					To get the list of commands in a specific category, type \`${msg.guild.settings.get(GuildSettings.Prefix)}commands <categoryName>\`.
-					To get help with a specific command, type \`${msg.guild.settings.get(GuildSettings.Prefix)}help <commandName>\`.
+					To get the list of command categories, type \`${prefix}commands\`.
+					To get the list of commands in a specific category, type \`${prefix}commands <categoryName>\`.
+					To get help with a specific command, type \`${prefix}help <commandName>\`.
 				`)
 			.addField('❯ Spudnik Command', '[Join](https://spudnik.io/support)', true)
 			.addField('❯ Invite to Your Server!', '[Invite](https://spudnik.io/invite)', true)
-			.setFooter(`Server Prefix: ${msg.guild.settings.get(GuildSettings.Prefix)} • Total Commands: ${this.client.commands.size}`);
+			.setFooter(`Server Prefix: ${prefix} • Total Commands: ${this.client.commands.size}`);
 
 		return msg.sendEmbed(helpEmbed);
 	}
