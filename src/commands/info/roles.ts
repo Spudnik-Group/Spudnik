523498ee73854ab0785d986d4c78128607c2ce30
Nate Richardson <hello@naterichardson.com>
2020-12-08 23:18:38 -0500

    fixed issue with self-assignable roles list getting big

--- src/commands/info/roles.ts
+++ src/commands/info/roles.ts
@@ -2,10 +2,10 @@
  * Copyright (c) 2020 Spudnik Group
  */
 
-import { MessageEmbed, Role } from 'discord.js';
-import { Command, CommandStore, KlasaMessage } from 'klasa';
+import { MessageEmbed, Role, User } from 'discord.js';
+import { Command, CommandStore, KlasaMessage, RichDisplay } from 'klasa';
 import { GuildSettings } from '@lib/types/settings/GuildSettings';
-import { specialEmbed, specialEmbedTypes } from '@lib/helpers/embed-helpers';
+import { specialEmbed, specialEmbedTypes, baseEmbed } from '@lib/helpers/embed-helpers';
 
 /**
  * Lists default and self-assignable roles.
@@ -29,34 +29,58 @@ export default class RolesCommand extends Command {
 	 * @returns {(Promise<KlasaMessage | KlasaMessage[]>)}
 	 * @memberof RolesCommand
 	 */
-	public run(msg: KlasaMessage): Promise<KlasaMessage | KlasaMessage[]> {
-		const roleEmbed: MessageEmbed = specialEmbed(msg, specialEmbedTypes.RoleManager);
+	public async run(msg: KlasaMessage): Promise<KlasaMessage | KlasaMessage[]> {
 		const guildAssignableRoles: string[] = msg.guild.settings.get(GuildSettings.Roles.SelfAssignable);
 		const guildDefaultRole: string = msg.guild.settings.get(GuildSettings.Tos.Role);
 
 		if (guildAssignableRoles.length) {
-			const rolesListOut: string[] = [];
+			if (guildAssignableRoles.length > 10) {
+				const display = new RichDisplay(baseEmbed(msg)
+					.setTitle('Self-Assignable Roles List'));
+				const noOfPages = Math.ceil(guildAssignableRoles.length / 10);
 
-			guildAssignableRoles.forEach((role: string) => {
-				const r: Role = msg.guild.roles.find((r: Role) => r.id === role.toString());
-				if (r) rolesListOut.push(`* <@&${r.id}> - ${r.members.size} members`);
-			});
+				for (let i = 0; i < noOfPages; i++) {
+					const currentPage = guildAssignableRoles.slice(i * 10, (i * 10) + 10);
+					display.addPage((template: MessageEmbed) => {
+						let output: string = '';
+						currentPage.forEach((roleID: string) => {
+							const r: Role = msg.guild.roles.find((r: Role) => r.id === roleID);
+							output += `* <@&${r.id}> - ${r.members.size} members
+							`;
+						});
+						template.setDescription(output);
 
-			if (rolesListOut.length) {
-				roleEmbed.addField('Assignable Roles', `${rolesListOut.sort().join('\n')}`, true);
+						return template;
+					});
+				}
+
+				await display.run(await msg.send('Loading server list...'), { filter: (_reaction: any, user: User) => user === msg.author });
+			} else {
+				const roleEmbed: MessageEmbed = specialEmbed(msg, specialEmbedTypes.RoleManager);
+				const rolesListOut: string[] = [];
+
+				guildAssignableRoles.forEach((role: string) => {
+					const r: Role = msg.guild.roles.find((r: Role) => r.id === role.toString());
+					if (r) rolesListOut.push(`* <@&${r.id}> - ${r.members.size} members`);
+				});
+
+				if (rolesListOut.length) {
+					roleEmbed.addField('Assignable Roles', `${rolesListOut.sort().join('\n')}`, true);
+				}
+
+				if (guildDefaultRole) {
+					roleEmbed.addField('Default (TOS) Role', `<@&${guildDefaultRole}>`, true);
+				}
+
+				if (Array.isArray(roleEmbed.fields) && roleEmbed.fields.length === 0) {
+					roleEmbed.setDescription('This guild does not have a default role or any self-assignable roles set.');
+				}
+
+				// Send the response
+				return msg.sendEmbed(roleEmbed);
 			}
 		}
 
-		if (guildDefaultRole) {
-			roleEmbed.addField('Default (TOS) Role', `<@&${guildDefaultRole}>`, true);
-		}
-
-		if (Array.isArray(roleEmbed.fields) && roleEmbed.fields.length === 0) {
-			roleEmbed.setDescription('This guild does not have a default role or any self-assignable roles set.');
-		}
-
-		// Send the response
-		return msg.sendEmbed(roleEmbed);
 	}
 
 }
